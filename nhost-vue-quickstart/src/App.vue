90a20a9a1081622b6cc9261faad274822c66e440
Kurt McAlpine <kurt@linux.com>
2024-05-23 12:25:52 +1200

    wip

--- nhost-vue-quickstart/src/App.vue
+++ nhost-vue-quickstart/src/App.vue
@@ -5,59 +5,102 @@ import { ref, onMounted } from 'vue'
 import { nhost } from "./lib/nhost"
 
 
-const getPumpsQuery = `
+//const getPumpsQuery = `
+//query GetPumps {
+//  Pumps(where: {Power: {_gte: "0", _lte: "9999"}, Weight: {_gte: "0", _lte: "9999"}, FlowRate: {_gte: "0", _lte: "9999"}}) {
+//    ID
+//    Power
+//    ProductName
+//    Weight
+//    FlowRate
+//    submersible
+//  }
+//}
+//`;
+const getPumpsQueryV2 = `
 query GetPumps {
-  Pumps(where: {Power: {_gte: "0", _lte: "9999"}, Weight: {_gte: "0", _lte: "9999"}, FlowRate: {_gte: "0", _lte: "9999"}}) {
-    ID
-    Power
-    ProductName
-    Weight
-    FlowRate
-    submersible
+  Pumps_v2(limit: 10, where: {PRESSURE_BAR: {_gte: "0", _lte: "100"}}) {
+    CC_STROKE
+    FLOAT_SWITCH
+    FLOW_L_H
+    HEAD_O_RING_EP
+    HEAD_O_RING_FP
+    HEAD_PP_SEAL_EP
+    HEAD_PVDF_SEAL_EP
+    HEAD_PVDF_SEAL_FP
+    INPUT
+    INPUT_TYPE
+    PRESSURE_BAR
+    PUMP_HEAD_CODE
+    PUMP_SERIES
+    STROKE_LENGTH_ADJ
+    STROKE_SPEED_ADJ
+    TUBE_SIZE_ID_X_OD
+    diaphragm
+    model
   }
-}
-`;
-
-function getQuery(flowRate: int[], power: int[], weight: int[], submersible: boolean) {
-console.log(submersible)
-return `
-query GetPumps {
-  Pumps(where: {Power: {_gte: "${power[0]}", _lte: "${power[1]}"}, Weight: {_gte: "${weight[0]}", _lte: "${weight[1]}"}, FlowRate: {_gte: "${flowRate[0]}", _lte: "${flowRate[1]}"}, submersible: {_eq: ${submersible}}}) {
-    ID
-    Power
-    ProductName
-    Weight
-    FlowRate
-    submersible
-  }
-}
-`;
-}
-
-async function getImageUrl(pumpSeries: string): string {
-  const { data } = await nhost.graphql.request(`query MyQuery {
-  files(where: {name: { _regex: "${pumpSeries}.*"}}) {
+  files {
     id
     name
   }
-}`)
-  const publicUrl = await nhost.storage.getPublicUrl({fileId: data.files[0].id})
-  return publicUrl
+}
+`;
+
+function getQuery(pressureBar: int[]) {
+return `
+query GetPumps {
+  Pumps_v2(limit: 10, where: {PRESSURE_BAR: {_gte: "${pressureBar[0]}", _lte: "${pressureBar[1]}"}}) {
+    CC_STROKE
+    FLOAT_SWITCH
+    FLOW_L_H
+    HEAD_O_RING_EP
+    HEAD_O_RING_FP
+    HEAD_PP_SEAL_EP
+    HEAD_PVDF_SEAL_EP
+    HEAD_PVDF_SEAL_FP
+    INPUT
+    INPUT_TYPE
+    PRESSURE_BAR
+    PUMP_HEAD_CODE
+    PUMP_SERIES
+    STROKE_LENGTH_ADJ
+    STROKE_SPEED_ADJ
+    TUBE_SIZE_ID_X_OD
+    diaphragm
+    model
+  }
+  files {
+    id
+    name
+  }
+}
+`;
 }
 
 var flowRate = [0, 1000];
+var pressureBar = [0, 100];
 var power = [0, 1000];
 var weight = [0, 1000];
 var submersible = false;
 
 const pumps = ref([])
 
-async function getPumps() {
-  const { data } = await nhost.graphql.request(getPumpsQuery)
-  for (var pump of data.Pumps) {
-     pump.PublicUrl = await getImageUrl("VCL")
+function appendPublicUrl(data) {
+  for (var pump of data.Pumps_v2) {
+    if (data.files != null) {
+      for (var file of data.files) {
+        if (pump.PUMP_SERIES + ".png" === file.name) {
+          pump.PublicUrl = "https://wkpurnuzgmelrnnrljvu.storage.ap-southeast-1.nhost.run/v1/files/" + file.id
+        }
+      }
+    }
   }
-  pumps.value = data.Pumps
+}
+
+async function getPumps() {
+  const { data } = await nhost.graphql.request(getPumpsQueryV2)
+  appendPublicUrl(data)
+  pumps.value = data.Pumps_v2
 }
 
 onMounted(() => {
@@ -65,11 +108,9 @@ onMounted(() => {
 })
 
 async function updatePumps() {
-  const { data } = await nhost.graphql.request(getQuery(flowRate, power, weight, submersible))
-  for (var pump of data.Pumps) {
-     pump.PublicUrl = await getImageUrl("VCL")
-  }
-  pumps.value = data.Pumps
+  const { data } = await nhost.graphql.request(getQuery(pressureBar))
+  appendPublicUrl(data)
+  pumps.value = data.Pumps_v2
 }
 
 function marks(val) {
@@ -87,10 +128,11 @@ function marks(val) {
         Specifications
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item"><p>Flow rate</p><vue-slider v-model="flowRate" :enable-cross="false" @change="updatePumps" :min="0" :max="1000" :interval="10" :marks="marks" ></vue-slider></li>
+        <li class="list-group-item"><p>Pressure BAR</p><vue-slider v-model="pressureBar" :enable-cross="false" @change="updatePumps" :min="0" :max="100" :interval="1"  ></vue-slider></li>
+        <!--<li class="list-group-item"><p>Flow rate</p><vue-slider v-model="flowRate" :enable-cross="false" @change="updatePumps" :min="0" :max="1000" :interval="10" :marks="marks" ></vue-slider></li>
         <li class="list-group-item"><p>Power</p><vue-slider v-model="power" :enable-cross="false" @change="updatePumps" :min="0" :max="1000" :interval="10" :marks="marks"></vue-slider></li>
         <li class="list-group-item"><p>Weight</p><vue-slider v-model="weight" :enable-cross="false" @change="updatePumps" :min="0" :max="1000" :interval="10" :marks="marks"></vue-slider></li>
-        <li class="list-group-item"><p>Submersible</p><input v-model="submersible" @change="updatePumps" type="checkbox"></li>
+        <li class="list-group-item"><p>Submersible</p><input v-model="submersible" @change="updatePumps" type="checkbox"></li>-->
       </ul>
     </div>
 
@@ -101,12 +143,30 @@ function marks(val) {
         <div class="card" style="width: 18rem;">
           <img v-bind:src="pump.PublicUrl" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">{{ pump.ProductName }}</h5>
-            <p class="card-text">Flow rate: {{ pump.FlowRate }}</p>
+            <h5 class="card-title">{{ pump.PUMP_SERIES }} {{ pump.model }}</h5>
+
+              <p class="card-text">CC_STROKE: {{ pump.CC_STROKE }}</p>
+              <p v-if="pump.FLOAT_SWITCH" class="card-text">FLOAT_SWITCH: {{ pump.FLOAT_SWITCH }}</p>
+              <p class="card-text">FLOW_L_H: {{ pump.FLOW_L_H }}</p>
+              <p class="card-text">HEAD_O_RING_EP: {{ pump.HEAD_O_RING_EP }}</p>
+              <p class="card-text">HEAD_O_RING_FP: {{ pump.HEAD_O_RING_FP }}</p>
+              <p class="card-text">HEAD_PP_SEAL_EP: {{ pump.HEAD_PP_SEAL_EP }}</p>
+              <p class="card-text">HEAD_PVDF_SEAL_EP: {{ pump.HEAD_PVDF_SEAL_EP }}</p>
+              <p class="card-text">HEAD_PVDF_SEAL_FP: {{ pump.HEAD_PVDF_SEAL_FP }}</p>
+              <p v-if="pump.INPUT" class="card-text">INPUT: {{ pump.INPUT }}</p>
+              <p v-if="pump.INPUT_TYPE" class="card-text">INPUT_TYPE: {{ pump.INPUT_TYPE }}</p>
+              <p class="card-text">PRESSURE_BAR: {{ pump.PRESSURE_BAR }}</p>
+              <p class="card-text">PUMP_HEAD_CODE: {{ pump.PUMP_HEAD_CODE }}</p>
+              <p v-if="pump.STROKE_LENGTH_ADJ" class="card-text">STROKE_LENGTH_ADJ: {{ pump.STROKE_LENGTH_ADJ }}</p>
+              <p class="card-text">STROKE_SPEED_ADJ: {{ pump.STROKE_SPEED_ADJ }}</p>
+              <p class="card-text">TUBE_SIZE_ID_X_OD: {{ pump.TUBE_SIZE_ID_X_OD }}</p>
+              <p class="card-text">diaphragm: {{ pump.diaphragm }}</p>
+
+            <!--<p class="card-text">Flow rate: {{ pump.FlowRate }}</p>
             <p class="card-text">Power: {{ pump.Power }}</p>
             <p class="card-text">Weight: {{ pump.Weight }}</p>
             <p v-if="pump.submersible" class="card-text">Submersible: yes</p>
-            <p v-else class="card-text">Submersible: no</p>
+            <p v-else class="card-text">Submersible: no</p>-->
           </div>
         </div>
       </div>
